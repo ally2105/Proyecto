eb6dec1269bd922ab9ffc3d076828d2b730c3052
ally2105 <allisonpauleth13@gmail.com>
2025-04-04 20:46:16 +0000

    cree lista de preguntas y respuestas

---
+++ proyecto.py
@@ -0,0 +1,17 @@
+preguntas = [
+    {
+        "pregunta": "¿cuánto es 15 + 5?: ",
+        "opciones": ["1- 5", "2- 6", "3- 9", "4- 20"],
+        "respuesta_correcta": "4" 
+    },
+    {
+        "pregunta": "¿cuál es el planeta en el que vivimos?: ",
+        "opciones": ["1- marte", "2- jupiter", "3- tierra", "4- saturno"],
+        "respuesta_correcta": "3"  
+    },
+    {
+        "pregunta": "¿que es lo que necesita las plantas para crecer?: ",
+        "opciones": ["1- agua y sol", "2- gaseosa", "3- clorox", "4- agua y aire"],
+        "respuesta_correcta": "1" 
+    }
+]
